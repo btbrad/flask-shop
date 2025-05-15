ddcbb3ccf1b256339abfd9ddc5c9f06fcdd69173
btbrad <24836323+btbrad@users.noreply.github.com>
2025-05-15 20:35:31 +0800

    菜单model创建

--- flask_shop/models.py
+++ flask_shop/models.py
@@ -25,3 +25,30 @@ class User(db.Model, BaseModel):
 
     def check_password(self, password):
         return check_password_hash(self.pwd, password)    
+    
+
+class Menu(db.Model):
+    __tablename__ = 't_menu'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(32),unique=True,nullable=False)
+    level = db.Column(db.Integer)
+    path = db.Column(db.String(32))
+    pid = db.Column(db.Integer)
+    children = db.relationship('Menu')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'level': self.level,
+            'path': self.path,
+            'pid': self.pid,
+            'children': self.get_child_list()
+        }
+    
+    def get_child_list(self):
+        obj_child = self.children
+        data = []
+        for o in obj_child:
+            data.append(o.to_dict())
+        return data    
